cb02fb1d2bdfc025c485e56ee3ee8951ad5592fb
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-04-11 08:53:36 -0600

    Deleted some of the functions in seed.js

--- seed.js
+++ seed.js
@@ -44,47 +44,29 @@ var pets_list = [
  }
 ];
 
-db.Pet.remove({}, function(err, authors) {
-  console.log('removed all authors');
-  db.Author.create(authors_list, function(err, authors){
+db.Pet.remove({}, function(err, pets) {
+  console.log('removed all pets');
+  db.Pet.create(pets_list, function(err, pets){
     if (err) {
       console.log(err);
       return;
     }
-    console.log('recreated all authors');
-    console.log("created", authors.length, "authors");
+    console.log('recreated all pets');
+    console.log("created", pets.length, "pets");
 
-
-    db.Book.remove({}, function(err, books){
-      console.log('removed all books');
-      books_list.forEach(function (bookData) {
-        var book = new db.Book({
-          title: bookData.title,
-          image: bookData.image,
-          releaseDate: bookData.releaseDate
+ db.myInfo.remove({}, function(err, myInfo){
+      console.log('removed all myInfo');
+      myInfo_list.forEach(function (myInfoData) {
+        var myInfo = new db.Book({
+          title: myInfoData.title,
+          image: myInfoData.image,
+          releaseDate: myInfoData.releaseDate
         });
-        db.Author.findOne({name: bookData.author}, function (err, foundAuthor) {
-          console.log('found author ' + foundAuthor.name + ' for book ' + book.title);
-          if (err) {
-            console.log(err);
-            return;
-          }
-          book.author = foundAuthor;
-          book.save(function(err, savedBook){
-            if (err) {
-              return console.log(err);
-            }
-            console.log('saved ' + savedBook.title + ' by ' + foundAuthor.name);
-          });
-        });
-      });
-    });
-
+    
+});
   });
 });
-
-
-
+});
 
 
 // db.Campsite.create(new_campsite, function(err, campsite){
